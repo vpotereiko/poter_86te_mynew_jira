9a0dc773062d7874103195af980c2fd0370aa40d
Administrator <admin@example.com>
2024-04-12 02:12:59 +0300

    menu as class #2

--- main_m.py
+++ main_m.py
@@ -2,9 +2,9 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import pprint
+from pprint import pprint
 
-from menu import Menu
+from menu import Menu, COLOR_OFF, COLOR_YELLOW
 
 
 # перевизначення дій в меню
@@ -57,63 +57,92 @@ def do_next_option(menu_id=1):
     print(f"GO BACK SELECTED WITH {menu_id=}")
 
 
-menu_id9 = {
-    1: option_1,
-    2: option_2,
-    3: option_3,
-    4: option_4,
-    5: option_5,
-    6: option_6,
-    7: option_7,
-    8: option_8,
-    9: option_9
-}
+self = None
 
+
+#
 
 class Project(Menu):
-    # перевизначення дій в меню
-    def option_1(self, menu_id=1):
-        print(f"P1 with {menu_id=}")
-        print()
+    def print_option_1(self, menu_id=1):
+        match menu_id:
+            case 1:
+                print(f"{COLOR_YELLOW}1:{COLOR_OFF} Create project.")
 
-    def option_2(self, menu_id=1):
-        print(f"P2 with {menu_id=}")
-        print()
+    def print_option_2(self, menu_id=1):
+        match menu_id:
+            case 1:
+                print(f"{COLOR_YELLOW}2:{COLOR_OFF} View all project.")
 
-    def option_3(self, menu_id=1):
-        print(f"P3 with {menu_id=}")
-        print()
+    def print_option_3(self, menu_id=1):
+        match menu_id:
+            case 1:
+                print(f"{COLOR_YELLOW}3:{COLOR_OFF} Edit project.")
 
-    def option_4(self, menu_id=1):
-        print(f"P4 with {menu_id=}")
-        print()
+    def print_option_4(self, menu_id=1):
+        match menu_id:
+            case 1:
+                print(f"{COLOR_YELLOW}4:{COLOR_OFF} Delete project.")
 
-    def option_5(self, menu_id=1):
+    def print_option_5(self, menu_id=1):
         print(f"P5 with {menu_id=}")
         print()
 
-    def option_6(self, menu_id=1):
+    def print_option_6(self, menu_id=1):
         print(f"P6 with {menu_id=}")
         print()
 
-    def option_7(self, menu_id=1):
+    def print_option_7(self, menu_id=1):
         print(f"P7 with {menu_id=}")
         print()
 
-    def option_8(self, menu_id=1):
+    def print_option_8(self, menu_id=1):
         print(f"P8 with {menu_id=}")
         print()
 
-    def option_9(self, menu_id=1):
+    def print_option_9(self, menu_id=1):
         print(f"P9 with {menu_id=}")
         print()
 
     def back_func(self, menu_id=1):
         do_next_option(menu_id)
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(self)
-        pprint.PrettyPrinter(f"{self.__dict__=}")
+    def __init__(self, menu_items, back_func):
+        super().__init__(menu_items, back_func)
+        pprint(f"110{self=}")
+        pprint(f"111{menu_items=}")
+        pprint(f"112{back_func=}")
+        pprint(f"{self.__dict__=}")
+
+    # default_menu = {
+    #     1: option_1(),
+    #     2: option_2(),
+    #     3: option_3(),
+    #     4: option_4(),
+    #     5: option_5(),
+    #     6: option_6(),
+    #     7: option_7(),
+    #     8: option_8(),
+    #     9: option_9()
+    # }
+
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, 'menu_items') or hasattr(cls, 'menu_items') and not cls.menu_items:
+            cls.menu_items = {
+                1: cls.print_option_1(self),
+                2: cls.print_option_2(self),
+                3: cls.print_option_3(self),
+                4: cls.print_option_4(self),
+                5: cls.print_option_5(self),
+                6: cls.print_option_6(self),
+                7: cls.print_option_7(self),
+                8: cls.print_option_8(self),
+                9: cls.print_option_9(self),
+            }
+            if not hasattr(cls, 'back_func') or hasattr(cls, 'back_func') and not cls.back_func:
+                cls.back_func = cls.back_func
+            if not hasattr(cls, '_instance'):
+                cls._instance = super().__new__(cls, cls.menu_items, cls.back_func)
+            return cls._instance
 
 
 def print_hi(name):
@@ -122,8 +151,23 @@ def print_hi(name):
 
 
 def testing_my_menu():
-    m1 = Menu(menu_id9, do_next_option)
-    m1.loop_of_menu()
+    # default_menu = {
+    #     1: print_option_1(self),
+    #     2: print_option_2(self),
+    #     3: print_option_3(self),
+    #     4: print_option_4(self),
+    #     5: print_option_5(self),
+    #     6: print_option_6(self),
+    #     7: print_option_7(self),
+    #     8: print_option_8(self),
+    #     9: print_option_9(self),
+    # }
+    menu_items = None
+    back_func = None
+    p1 = Project(menu_items, back_func)
+    p1.loop_of_menu()
+    # m1 = Menu(default_menu, do_next_option)
+    # m1.loop_of_menu()
 
 
 # Press the green button in the gutter to run the script.
--- menu.py
+++ menu.py
@@ -1,28 +1,30 @@
-color = {
-    'white': "\033[1;37m",
-    'yellow': "\033[1;33m",
-    'green': "\033[1;32m",
-    'blue': "\033[1;34m",
-    'cyan': "\033[1;36m",
-    'red': "\033[1;31m",
-    'magenta': "\033[1;35m",
-    'black': "\033[1;30m",
-    'darkwhite': "\033[0;37m",
-    'darkyellow': "\033[0;33m",
-    'darkgreen': "\033[0;32m",
-    'darkblue': "\033[0;34m",
-    'darkcyan': "\033[0;36m",
-    'darkred': "\033[0;31m",
-    'darkmagenta': "\033[0;35m",
-    'darkblack': "\033[0;30m",
-    'off': "\033[0;0m"
-}
 
-yellow = "\033[1;33m"
-off = "\033[0;0m"
+from pprint import pprint
 
 
-# перевизначення дій в меню
+COLOR = {
+        'white': "\033[1;37m",
+        'yellow': "\033[1;33m",
+        'green': "\033[1;32m",
+        'blue': "\033[1;34m",
+        'cyan': "\033[1;36m",
+        'red': "\033[1;31m",
+        'magenta': "\033[1;35m",
+        'black': "\033[1;30m",
+        'darkwhite': "\033[0;37m",
+        'darkyellow': "\033[0;33m",
+        'darkgreen': "\033[0;32m",
+        'darkblue': "\033[0;34m",
+        'darkcyan': "\033[0;36m",
+        'darkred': "\033[0;31m",
+        'darkmagenta': "\033[0;35m",
+        'darkblack': "\033[0;30m",
+        'off': "\033[0;0m"
+    }
+COLOR_YELLOW = "\033[1;33m"
+COLOR_OFF = "\033[0;0m"
+
+
 def option_1(menu_id=1):
     print(f"P1 with {menu_id=}")
     print()
@@ -71,22 +73,21 @@ def option_9(menu_id=1):
 def do_next_option(menu_id=1):
     print(f"GO BACK SELECTED WITH {menu_id=}")
 
-
-menu_id9 = {
-    1: option_1,
-    2: option_2,
-    3: option_3,
-    4: option_4,
-    5: option_5,
-    6: option_6,
-    7: option_7,
-    8: option_8,
-    9: option_9
-}
+#
+# menu_id9 = {
+#     1: option_1,
+#     2: option_2,
+#     3: option_3,
+#     4: option_4,
+#     5: option_5,
+#     6: option_6,
+#     7: option_7,
+#     8: option_8,
+#     9: option_9
+# }
 
 
 # class Project(Menu):
-#     # перевизначення дій в меню
 #     def option_1(self, menu_id=1):
 #         print(f"P1 with {menu_id=}")
 #         print()
@@ -131,47 +132,105 @@ menu_id9 = {
 #         pprint.PrettyPrinter(f"{self.__dict__=}")
 #
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-def testing_my_menu():
-    pass
-
-
 class Menu:
-    owner = None
+    """
+    A class that represents a menu
+    """
+    # def __new__(cls, *args, **kwargs):
+    #     pprint(f"{cls=}")
+    #     pprint(f"{args=}")
+    #     pprint(f"{kwargs=}")
+    #     return super().__new__(cls, *args, **kwargs)
+    # def option_1(self, menu_id=1):
+    #     pass # self.owner.option_1(menu_id)
+    #
+    # def option_2(self, menu_id=1):
+    #     pass # self.owner.option_2(menu_id)
+    #
+    # def option_3(self, menu_id=1):
+    #     pass # self.owner.option_3(menu_id)
+    #
+    # def option_4(self, menu_id=1):
+    #     pass # self.owner.option_4(menu_id)
+    #
+    # def option_5(self, menu_id=1):
+    #     pass # self.owner.option_5(menu_id)
+    #
+    # def option_6(self, menu_id=1):
+    #     pass # self.owner.option_6(menu_id)
+    #
+    # def option_7(self, menu_id=1):
+    #     pass # self.owner.option_7(menu_id)
+    #
+    # def option_8(self, menu_id=1):
+    #     pass # self.owner.option_8(menu_id)
+    #
+    # def option_9(self, menu_id=1):
+    #     pass # self.owner.option_9(menu_id)
+    #
+    # def back_func(self, menu_id=1):
+    #     pass # self.owner.back_func(menu_id)
 
-    def option_1(self, menu_id=1):
-        self.owner.option_1(menu_id)
+    def __new__(cls, menu_items, back_func):
+        pprint(f"174{cls=}")
+        pprint(f"175{menu_items=}")
+        pprint(f"176{back_func=}")
+        return super().__new__(cls)
 
-    def option_2(self, menu_id=1):
-        self.owner.option_2(menu_id)
+    def __init__(self, menu_items, back_func):
+        pprint(f"180{self=}")
+        pprint(f"181{menu_items=}")
+        pprint(f"182{back_func=}")
+        # self.options = {
+        #     1: self.option_1(),
+        #     2: self.option_2(),
+        #     3: self.option_3(),
+        #     4: self.option_4(),
+        #     5: self.option_5(),
+        #     6: self.option_6(),
+        #     7: self.option_7(),
+        #     8: self.option_8(),
+        #     9: self.option_9()
+        # }
+        # self.owner = None
+        self.back_func = back_func
+        self.menu_items = menu_items
+        # # self.selected_item = 0
+        self.last_item = int(len(self.menu_items)) + 1
 
-    def option_3(self, menu_id=1):
-        self.owner.option_3(menu_id)
+    def show_menu(self, item=1,):
+        if (item == 1):
+            print(f"{yellow}{self.__class__} menu {off}")
+        print(f"{yellow}{item}: {off} {self.menu_items[item].__name__}")
 
-    def option_4(self, menu_id=1):
-        self.owner.option_4(menu_id)
+        if item == len(self.menu_items):
+            print(f"{color_off}{self.last_item}{color_off} Exit")
+            pass
+        else:
+            return self.show_menu(item + 1)
 
-    def option_5(self, menu_id=1):
-        self.owner.option_5(menu_id)
-
-    def option_6(self, menu_id=1):
-        self.owner.option_6(menu_id)
-
-    def option_7(self, menu_id=1):
-        self.owner.option_7(menu_id)
-
-    def option_8(self, menu_id=1):
-        self.owner.option_8(menu_id)
-
-    def option_9(self, menu_id=1):
-        self.owner.option_9(menu_id)
-
-    def back_func(self, menu_id=1):
-        self.owner.back_func(menu_id)
+    def loop_of_menu(self):
+        self.show_menu()
+        try:
+            print(f"{color['darkblue']}{'-' * 5 * len(self.menu_items)}{off}")
+            self.selected_item = int(input("Enter your choice"))
+            print(f"{color['darkblue']}{'-' * 5 * len(self.menu_items)}{off}")
+            if self.selected_item < 1 or self.selected_item > self.last_item:
+                print(f"Your choice out of range (1-{self.last_item}")
+                return self.loop_of_menu()
+            else:
+                if self.selected_item == self.last_item:
+                    if callable(self.back_func):
+                        self.back_func()
+                        pass
+                    else:
+                        print(f"{color['darkgreen']}{'-' * 5 * len(self.menu_items)}{off}")
+                        self.menu_items[str(self.selected_item)]()
+                        print(f"{color['darkgreen']}{'-' * 5 * len(self.menu_items)}{off}")
+                        return self.loop_of_menu()
+        except Exception as e:
+            print(f"Error - try again:{str(e)}")
+            return self.loop_of_menu()
 
     # @property
     # def options(self):
@@ -276,14 +335,14 @@ class Menu:
 
     def show_menu(self, item=1):
         if item == 1:
-            print(f"{yellow}{self.owner.__class__} menu {off}")
-        print(f"{yellow}{item}: {off} {self.menu_items[item].__name__}")
+            print(f"{COLOR_YELLOW}{self.__class__} menu {COLOR_OFF}")
+        print(f"{COLOR_YELLOW}{item}: {COLOR_OFF} {self.menu_items[item].__name__}")
 
         if item == len(self.menu_items):
-            print(f"{yellow}{self.last_item}{off} Exit")
+            print(f"{COLOR_YELLOW}{self.last_item}{COLOR_OFF} Exit")
             pass
         else:
-            return self.show_menu(item + 1)  # виведення меню рекурсивно
+            return self.show_menu(item + 1)
 
     def loop_of_menu(self):
         self.show_menu()
